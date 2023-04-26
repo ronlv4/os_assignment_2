dc637c600c7a30affac9ff156c89c8664e6193c9
Ron Levi <ronlv4@gmail.com>
2023-04-26 19:04:50 +0000

    adding new system calls without impl

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -86,6 +86,11 @@ extern uint64 sys_wait(void);
 extern uint64 sys_pipe(void);
 extern uint64 sys_read(void);
 extern uint64 sys_kill(void);
+extern uint64 sys_kthread_create(void);
+extern uint64 sys_kthread_exit(void);
+extern uint64 sys_kthread_id(void);
+extern uint64 sys_kthread_join(void);
+extern uint64 sys_kthread_kill(void);
 extern uint64 sys_exec(void);
 extern uint64 sys_fstat(void);
 extern uint64 sys_chdir(void);
@@ -105,27 +110,32 @@ extern uint64 sys_close(void);
 // An array mapping syscall numbers from syscall.h
 // to the function that handles the system call.
 static uint64 (*syscalls[])(void) = {
-[SYS_fork]    sys_fork,
-[SYS_exit]    sys_exit,
-[SYS_wait]    sys_wait,
-[SYS_pipe]    sys_pipe,
-[SYS_read]    sys_read,
-[SYS_kill]    sys_kill,
-[SYS_exec]    sys_exec,
-[SYS_fstat]   sys_fstat,
-[SYS_chdir]   sys_chdir,
-[SYS_dup]     sys_dup,
-[SYS_getpid]  sys_getpid,
-[SYS_sbrk]    sys_sbrk,
-[SYS_sleep]   sys_sleep,
-[SYS_uptime]  sys_uptime,
-[SYS_open]    sys_open,
-[SYS_write]   sys_write,
-[SYS_mknod]   sys_mknod,
-[SYS_unlink]  sys_unlink,
-[SYS_link]    sys_link,
-[SYS_mkdir]   sys_mkdir,
-[SYS_close]   sys_close,
+[SYS_fork]              sys_fork,
+[SYS_exit]              sys_exit,
+[SYS_wait]              sys_wait,
+[SYS_pipe]              sys_pipe,
+[SYS_read]              sys_read,
+[SYS_kill]              sys_kill,
+[SYS_kthread_create]    sys_kthread_create,
+[SYS_kthread_exit]      sys_kthread_exit,
+[SYS_kthread_id]        sys_kthread_id,
+[SYS_kthread_join]      sys_kthread_join,
+[SYS_kthread_kill]      sys_kthread_kill,
+[SYS_exec]              sys_exec,
+[SYS_fstat]             sys_fstat,
+[SYS_chdir]             sys_chdir,
+[SYS_dup]               sys_dup,
+[SYS_getpid]            sys_getpid,
+[SYS_sbrk]              sys_sbrk,
+[SYS_sleep]             sys_sleep,
+[SYS_uptime]            sys_uptime,
+[SYS_open]              sys_open,
+[SYS_write]             sys_write,
+[SYS_mknod]             sys_mknod,
+[SYS_unlink]            sys_unlink,
+[SYS_link]              sys_link,
+[SYS_mkdir]             sys_mkdir,
+[SYS_close]             sys_close,
 };
 
 void
